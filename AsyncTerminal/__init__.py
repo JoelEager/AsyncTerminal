19d9d87e3ba4ca850f1ff6392435f0a169191ccd
Joel Eager <jee7291@rit.edu>
2017-09-02 18:10:44 -0700

    Added version check

--- AsyncTerminal/__init__.py
+++ AsyncTerminal/__init__.py
@@ -1,1 +1,7 @@
+# Check Python version
+import sys
+if sys.version_info[0] < 3 or sys.version_info[1] < 5:
+    print("Python 3.5 or newer is required to run AsyncTerminal")
+    sys.exit(1)
+    
 from .AsyncTerminal import *
